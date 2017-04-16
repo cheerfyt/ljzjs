bce26f30d45d3a1684fbe81319364eec30f32f73
YT <yongtao.fan@yunshipei.com>
2017-04-17 00:31:55 +0800

    init

---
+++ test/test_sort.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const sort = require("../lib/sort");
+const {test} = require("ava");
+
+const bubble = sort.bubbleSort;
+const insert = sort.insertSort;
+
+const src = [
+  2,
+  1,
+  24,
+  17,
+  10,
+  9,
+  3,
+  4,
+  89,
+  55
+];
+const sorted = [
+  1,
+  2,
+  3,
+  4,
+  9,
+  10,
+  17,
+  24,
+  55,
+  89
+];
+
+test('1. #bubble sort ', t => {
+  t.deepEqual(bubble(src), sorted);
+});
+
+test('2. #insert sort', t => {
+  t.deepEqual(insert(src), sorted);
+});
